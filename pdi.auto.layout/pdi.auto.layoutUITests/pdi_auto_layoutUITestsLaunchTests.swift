ae4d91bcb17e120f6c5895a030842eb4fd8da5a5
guigrds <guilherme.rodrigues@inchurch.com.br>
2023-01-09 17:44:22 -0300

    Create files

---
+++ pdi.auto.layout/pdi.auto.layoutUITests/pdi_auto_layoutUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pdi_auto_layoutUITestsLaunchTests.swift
+//  pdi.auto.layoutUITests
+//
+//  Created by Guilherme Rodrigues on 09/01/23.
+//
+
+import XCTest
+
+final class pdi_auto_layoutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
