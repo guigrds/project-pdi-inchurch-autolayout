db8728bdcefbaacbfeec77b04df72fca762b62c4
guigrds <guilherme.rodrigues@inchurch.com.br>
2023-01-09 18:26:19 -0300

    Adicionando constraints

--- pdi.auto.layout/pdi.auto.layout/ViewController.swift
+++ pdi.auto.layout/pdi.auto.layout/ViewController.swift
@@ -1,1 +1,18 @@
-import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let imageView = UIImageView(image: koromon1)
+        view?.addSubview(imageView)
+        
+        imagemView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+}
