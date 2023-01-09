c62f0c6424044f64e8735a79a20850590506f63b
guigrds <guilherme.rodrigues@inchurch.com.br>
2023-01-09 19:41:44 -0300

    Limpeza do código retirando linhas commitadas

--- pdi.auto.layout/pdi.auto.layout/ViewController.swift
+++ pdi.auto.layout/pdi.auto.layout/ViewController.swift
@@ -2,18 +2,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var koromonimageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "koromon1"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let imageView = UIImageView(image: UIImage(named: "koromon1"))
-        view?.addSubview(imageView)
+        view?.addSubview(koromonimageView)
+    }
         
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        private func setuplayout(){
+            koromonimageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            koromonimageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+            koromonimageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            koromonimageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
 }
-
