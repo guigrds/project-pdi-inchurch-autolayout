95c5836bce963e76216f17abba1ae6b36211e258
guigrds <guilherme.rodrigues@inchurch.com.br>
2023-01-09 17:08:50 -0300

    Create project

---
+++ pdi.auto.layout/pdi.auto.layout/pdi_auto_layoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdi_auto_layoutApp.swift
+//  pdi.auto.layout
+//
+//  Created by Guilherme Rodrigues on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct pdi_auto_layoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
